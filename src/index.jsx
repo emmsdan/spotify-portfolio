cef9c9f57e0a7408e5a2ee566856b33307e8bed7
Emmanuel Daniel (emmsdan) <emmanuel.daniel@suretree.com>
2024-02-27 11:09:58 +0100

    folder structure base

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from './app/App';
+import reportWebVitals from './setup/reportWebVitals';
 
 const rootDom = document.getElementById('root')
 const root = ReactDOM.createRoot(rootDom);
