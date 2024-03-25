4aa326dfa6c9c1c293308f54e8788432cd15ea9a
Emmanuel Daniel (emmsdan) <emmanuel.daniel@suretree.com>
2024-03-25 13:48:49 +0100

    Dashboard layout

--- src/app/pages/Dashboard/index.jsx
+++ src/app/pages/Dashboard/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-
+import style from "./style.module.css"
 export const DashboardPage = () => {
-    return <>Dash</>
+    return (
+    <section className={style.container}>
+        <section  className={style['content-wrapper']} role='content'>
+            <aside>aside</aside>
+            <main>main</main>
+        </section>
+        
+        <footer className={style.footer}>footer</footer>
+    </section>)
 }
