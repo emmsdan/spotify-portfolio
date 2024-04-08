8f06acfbb214aaedee962989217768e3eec5a83a
Emmanuel Daniel <emmsdan.inc@gmail.com>
2024-04-08 11:25:54 +0100

    Updated to use server endpoint

--- src/app/service/songs.js
+++ src/app/service/songs.js
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker"
 import { routes } from "../routes"
 
 export const mostListenedToApi = async ()=>{
-    const resp = await fetch('http://localhost:3001/songs');
+    const resp = await fetch('https://spotify-test-api.onrender.com/songs');
     const data = await resp.json()
     return data.splice(0,8)
 }
