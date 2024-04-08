ccdf61980b6f8a845103168369849be7f9b0852a
Emmanuel Daniel <emmsdan.inc@gmail.com>
2024-04-08 12:00:37 +0100

    added env file

--- src/app/service/songs.js
+++ src/app/service/songs.js
@@ -1,13 +1,14 @@
 import { faker } from "@faker-js/faker"
 import { routes } from "../routes"
+import { env } from "../../env";
 
 export const mostListenedToApi = async ()=>{
-    const resp = await fetch('https://spotify-test-api.onrender.com/songs');
+    const resp = await fetch(`${env.BASE_API}/songs`);
     const data = await resp.json()
     return data.splice(0,8)
 }
 
 export const episodesApi = async ()=>{
-    const resp = await fetch('http://localhost:3001/songs');
+    const resp = await fetch(`${env.BASE_API}/songs`);
     return resp.json()
 }
